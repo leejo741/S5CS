46d939c50e378d064b01fe3b7399524aa041c40e
leejo <leejo@222-002.plasma.utschools.ca>
2018-11-15 11:15:58 -0500

    its lit

--- src/JohnLee_220.java
+++ src/JohnLee_220.java
@@ -1,45 +1,30 @@
 /*
  * This program tests the insertion sort
  */
+
+import java.util.*;
+
 public class JohnLee_220 
 {
 	public static void main(String[] args)
 	{
+		int maxSize = 4;
+		int minSize = 0;
 		// create an array
-		int[] data = new int[100] ;
+		int[] data = new int[maxSize] ;
 		
-		// fill the array with random numbers
-		for( int i = 0 ; i< data.length ; i++)
-		{
-			data[i] = (int)(Math.random()*1000); //the range of the numbers is 0-1000
-		}
+		randomize (data, minSize, maxSize) ; // same order as ints declared below
 		// print the array
-		print( data, 7) ;
+		print( data, maxSize) ;
 		// sort the array using insertion sort
-		insertionSort( data) ;
+		shuffle(data);
+		//insertionSort( data) ;
 		// print the sorted array
-		print( data, 7) ;
+		print( data, maxSize) ;
 	}
 	//this method sorts an array using the insertion algorithm
-	public static void insertionSort(int[] data)
-	{
-		if(data == null || data.length <2) return;
-		{
-			int first = 1; // the index of the first element in the unsorted part
-			while ( first < data.length)
-			{
-				// insert the first element from the unsorted part into its correct position in the sorted part
-				for ( int i = first ; i>0 && data[i] <= data[i-1] ; i--)
-				{
-					swap(data, i, i-1) ;
-				}
-				
-				// increase the sorted part / decrease the unsorted part
-				first ++;
-				
-			}
-		}
-	}
+	
+	
 	
 	static public void swap ( int [ ] b,int n, int m )
 	{
@@ -66,4 +51,62 @@ public class JohnLee_220
 			System.out.printf( "  %6d", data[ i ] ) ; // formatted output
 		System.out.print("\n\n") ; // separate from next output
 	}
+
+	// 221
+	public static void randomize ( int[] data, int lower, int upper)
+	{
+		for (int i = 0; i < data.length ; i++)
+		{
+			data[i] = lower + (int)(Math.random()*(upper - lower)+1); 
+		}
+		
+	}
+	
+	// 222
+	
+	public static void shuffle( int[] data )
+	{
+		int temp = 0;
+		int ind = 0;
+		for (int i = 0; i < data.length ; i++)
+		{
+			ind = (int)Math.random()*(data.length); //math.random gives value from 0-1 and int always rounds down
+			temp = data[ind];
+			data[ind] = data[i];
+			data[i] = temp;
+		}
+	}
+	
+	//223
+	
+	public static void reverse( int[] data )
+	{
+		for (int i = 0; i < data.length/2; i++)
+		{
+			data[i] = data[data.length - i];
+		}
+	}
+	
+	// 229
+	public static void insertionSort(int[] data)
+	{
+		if(data == null || data.length <2) return;
+		{
+			int first = 1; // the index of the first element in the unsorted part
+			while ( first < data.length)
+			{
+				// insert the first element from the unsorted part into its correct position in the sorted part
+				for ( int i = first ; i>0 && data[i] <= data[i-1] ; i--)
+				{
+					swap(data, i, i-1) ;
+				}
+				
+				// increase the sorted part / decrease the unsorted part
+				first ++;
+				
+			}
+		}
+	}
 }
+	
+	
