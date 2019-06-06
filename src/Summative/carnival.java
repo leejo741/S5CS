8b5a592ab36f659f2a9785b2c5c5d1a2f1231b6f
leejo <leejo@214B-009.plasma.utschools.ca>
2019-06-06 11:17:02 -0400

    come clutch

--- src/Summative/carnival.java
+++ src/Summative/carnival.java
@@ -12,6 +12,10 @@ public class carnival {
 		System.out.println("Please enter your account balance: ");
 		int money = inp.nextInt();
 		player john = new player(money);
+		gameBooth redBlack = new redBlack();
+		gameBooth pennyToss = new pennyToss();
+		gameBooth diceGame = new diceGame();
+		//add a while loop
 		System.out.println("Choose one of the options:");
 		System.out.println("(G) Play a Game");
 		System.out.println("(P) See Prizes");
@@ -24,20 +28,25 @@ public class carnival {
 			System.out.println("Which game would you like to play?");
 			System.out.println("(1) Red or Black");
 			System.out.println("(2) Penny Throw");
-			System.out.println("(3) GAME NOT YET CHOSEN");
+			System.out.println("(3) Dice Game");
 			System.out.println("Enter your choice: ");
 			int choice = inp.nextInt();
 			String gameChoice = null;
 			if (choice == 1) {
-			gameChoice = "Red or Black";	
+			gameChoice = "Red or Black";
+			System.out.println("Playing " + gameChoice) ;
+			redBlack.start();
 			}
 			if (choice == 2) {
-			gameChoice = "Penny Throw";		
+			gameChoice = "Penny Throw";	
+			System.out.println("Playing " + gameChoice) ;
 			}
 			if (choice == 3) {
-			gameChoice = "GAME NOT YET CHOSEN";		
-			}
+			gameChoice = "Dice Game";		
 			System.out.println("Playing " + gameChoice) ;
+			diceGame.start();
+			}
+			
 		}
 		
 		if (selection.equals("P")|| selection.equals("p")) {
@@ -70,27 +79,79 @@ abstract class gameBooth{
 }
 
 class redBlack extends gameBooth {
-
-	Scanner inp = new Scanner(System.in);
 	
-	this.cost = 1.5;
 	
 	public void start() {
 		
-		for (int k = 0; k < 3; k++) {
+		Scanner inp = new Scanner(System.in) ;
+		
+		this.cost = 1.5;
+		int cnt = 0;
+		for (int k = 0; k  <=2; k++) {
 			
-			int colour = (int) ((Math.random())*(2));
+			int colour = (int) ((Math.random())*(3));
 			
-			if (colour == 1) {
+//			System.out.println(colour);
+			if (colour <= 1) {
 				System.out.println("Black");
+				cnt ++;
 			}
-			if (colour == 2) {
+			if (colour > 1) {
 				System.out.println("Red");
 			}
 			
+			if (cnt == 2 || cnt == 0){
+				
+			}
+			
+		}	
+	}
+}
+
+class pennyToss extends gameBooth{
+	
+	String [][] board;
+	public void start (){
+		board = new String[][]{{"Poster", "Poster", "Plush Tiger", ""},
+							   {"", "Plush Tiger", "", "Poster"},
+			                   {"Poster","","Poster",""},
+			                   {"","","","Plush Tiger"}};
+	}
+}
+
+class diceGame extends gameBooth{
+	public void start(){
+	// user can guess greater than or less than (small prize) or three of a kind (large prize), make a menu option, output of rules and how to win	
+		System.out.println(" Welcome to Dice Game! ");
+		Scanner inp = new Scanner(System.in) ;
+		System.out.println("Enter your guess between 3 and 18: ");
+		int guess = inp.nextInt();
+		System.out.println("Rolling...");
+		
+		int one = (int) ((Math.random())*(6) +1);
+		System.out.println("Press enter to roll");
+		inp.nextLine();
+		System.out.println("Your first number is: "+ one);
+		
+		int two = (int) ((Math.random())*(6) +1);
+		System.out.println("Press enter to roll");
+		inp.nextLine();
+		System.out.println("Your second number is: "+ two);
+		
+		int three = (int) ((Math.random())*(6) +1);
+		System.out.println("Press enter to roll");
+		inp.nextLine();
+		System.out.println("Your third number is: "+ three);
+		
+		System.out.println("\nYour total sum is: " + (one + two + three));
+		
+		if (one + two + three == guess){
+			System.out.println("Congratulations! You won a huge plush hotdog!");
+			
+		}
+		else{
+			System.out.println("Please try again.");
 		}
 		
 	}
-	
-
 }
